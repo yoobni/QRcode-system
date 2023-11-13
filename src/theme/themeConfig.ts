a03c6d44fb7d567c584ccf0d7c4a32134c304838
ravi <ravi.go@handys.co.kr>
2023-11-13 16:13:38 +0900

    Feat: Add qr page

---
+++ src/theme/themeConfig.ts
@@ -0,0 +1,10 @@
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+    token: {
+        fontSize: 14,
+        // colorPrimary: '#900020',
+    },
+};
+
+export default theme;
